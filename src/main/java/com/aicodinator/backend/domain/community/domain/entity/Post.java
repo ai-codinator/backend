e14acd86298316b9e5271dda1e94f0e6b4d6f703
Yooonjeong <wkddbs3935@naver.com>
2025-09-24 21:53:32 +0900

    feat: Post 엔티티 컬럼 추가

--- src/main/java/com/aicodinator/backend/domain/community/domain/entity/Post.java
+++ src/main/java/com/aicodinator/backend/domain/community/domain/entity/Post.java
@@ -1,5 +1,7 @@
 package com.aicodinator.backend.domain.community.domain.entity;
 
+import com.aicodinator.backend.domain.region.domain.Region;
+import com.aicodinator.backend.domain.user.domain.entity.User;
 import com.aicodinator.backend.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
@@ -22,18 +24,60 @@ public class Post extends BaseEntity {
     @JoinColumn(name = "board_id")
     private Board board;
 
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "region_id")
+    private Region region;
+
     @Column(nullable = false)
     private String title;
 
     @Column(length = 1000) // 글자수 체크
     private String content;
 
+    private int likeCount = 0;
+
+    private int commentCount = 0;
+
+    private int viewCount = 0;
+
     @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
     @Builder.Default
     private List<PostFile> files = new ArrayList<>();
 
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private List<Comment> comments = new ArrayList<>();
+
     public void addFile(PostFile postFile) {
         files.add(postFile);
         postFile.setPost(this);
+    }
+
+    public void increaseLikeCount() {
+        this.likeCount++;
+    }
+
+    public void decreaseLikeCount() {
+        this.likeCount--;
+    }
+
+    public void increaseCommentCount() {
+        this.commentCount++;
+    }
+
+    public void decreaseCommentCount() {
+        this.commentCount--;
+    }
+
+    public void increaseViewCount() {
+        this.viewCount++;
+    }
+
+    public void decreaseViewCount() {
+        this.viewCount--;
     }
 }
