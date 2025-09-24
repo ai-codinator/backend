fcec91dcca5999663f0375a42d8f9918d7410850
Yooonjeong <wkddbs3935@naver.com>
2025-09-24 21:52:51 +0900

    feat: Post 생성 및 수정 Request DTO 정의

---
+++ src/main/java/com/aicodinator/backend/domain/community/domain/dto/request/PostEditRequest.java
@@ -0,0 +1,26 @@
+package com.aicodinator.backend.domain.community.domain.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+public class PostEditRequest {
+    @NotBlank(message = "게시글 제목은 필수입니다.")
+    @Size(max = 255, message = "제목은 255자 이내로 입력해주세요.")
+    private String title;
+
+    @Size(max = 1000, message = "게시글 내용은 1000자 이내로 작성해주세요.")
+    private String content;
+
+    private List<Long> deleteFileIds;
+}
