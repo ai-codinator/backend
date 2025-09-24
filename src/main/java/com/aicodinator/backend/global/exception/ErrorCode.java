372604133b4cccdba0dd9b6ecf7fbaff4a52f3a5
Yooonjeong <wkddbs3935@naver.com>
2025-09-24 21:44:25 +0900

    feat: ErrorCode 정의

--- src/main/java/com/aicodinator/backend/global/exception/ErrorCode.java
+++ src/main/java/com/aicodinator/backend/global/exception/ErrorCode.java
@@ -7,42 +7,46 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
-  /*
-   * 400 BAD_REQUEST: 잘못된 요청
-   */
-  BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+    /*
+     * 400 BAD_REQUEST: 잘못된 요청
+     */
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+    INVALID_FILE_REQUEST(HttpStatus.BAD_REQUEST, "유효하지 않은 파일 요청입니다."),
 
-  /*
-   * 401 UNAUTHORIZED: 인증되지 않은 사용자의 요청
-   */
-  UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증되지 않은 사용자입니다."),
-  INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 리프레시 토큰입니다."),
-  TOKEN_ALREADY_LOGOUT(HttpStatus.UNAUTHORIZED, "이미 로그아웃된 토큰입니다."),
-  TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "토큰이 만료되었습니다."),
-  INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "아이디 또는 비밀번호가 올바르지 않습니다."),
-  INCORRECT_CURRENT_PASSWORD(HttpStatus.UNAUTHORIZED, "현재 비밀번호가 일치하지 않습니다."),
+    /*
+     * 401 UNAUTHORIZED: 인증되지 않은 사용자의 요청
+     */
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증되지 않은 사용자입니다."),
+    INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 리프레시 토큰입니다."),
+    TOKEN_ALREADY_LOGOUT(HttpStatus.UNAUTHORIZED, "이미 로그아웃된 토큰입니다."),
+    TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "토큰이 만료되었습니다."),
+    INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "아이디 또는 비밀번호가 올바르지 않습니다."),
+    INCORRECT_CURRENT_PASSWORD(HttpStatus.UNAUTHORIZED, "현재 비밀번호가 일치하지 않습니다."),
 
-  /*
-   * 403 FORBIDDEN: 권한이 없는 사용자의 요청
-   */
-  FORBIDDEN(HttpStatus.FORBIDDEN, "접근 권한이 없습니다."),
+    /*
+     * 403 FORBIDDEN: 권한이 없는 사용자의 요청
+     */
+    FORBIDDEN(HttpStatus.FORBIDDEN, "접근 권한이 없습니다."),
 
-  /*
-   * 404 NOT_FOUND: 리소스를 찾을 수 없음
-   */
-  NOT_FOUND(HttpStatus.NOT_FOUND, "요청하신 리소스를 찾을 수 없습니다."),
+    /*
+     * 404 NOT_FOUND: 리소스를 찾을 수 없음
+     */
+    NOT_FOUND(HttpStatus.NOT_FOUND, "요청하신 리소스를 찾을 수 없습니다."),
 
-  /*
-   * 405 METHOD_NOT_ALLOWED: 허용되지 않은 Request Method 호출
-   */
-  METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "허용되지 않은 요청 방식입니다."),
+    /*
+     * 405 METHOD_NOT_ALLOWED: 허용되지 않은 Request Method 호출
+     */
+    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "허용되지 않은 요청 방식입니다."),
 
-  /*
-   * 500 INTERNAL_SERVER_ERROR: 내부 서버 오류
-   */
-  INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다.");
+    /*
+     * 500 INTERNAL_SERVER_ERROR: 내부 서버 오류
+     */
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다."),
+    S3_UPLOAD_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "S3 파일 업로드에 실패했습니다."),
+    S3_UPLOAD_AMAZON_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "S3 에러로 인해 파일 업로드에 실패했습니다."),
+    S3_DELETE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "S3 파일 삭제에 실패했습니다."),
+    S3_DELETE_AMAZON_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "S3 에러로 인해 파일 삭제에 실패했습니다.");
 
-
-  private final HttpStatus httpStatus;
-  private final String message;
+    private final HttpStatus httpStatus;
+    private final String message;
 }
