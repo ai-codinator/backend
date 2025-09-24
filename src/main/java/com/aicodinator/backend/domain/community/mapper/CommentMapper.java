bc100fe3718e552b963c6ef06a6af02712bda14c
Yooonjeong <wkddbs3935@naver.com>
2025-09-24 21:49:48 +0900

    feat: CommentMapper 정의

---
+++ src/main/java/com/aicodinator/backend/domain/community/mapper/CommentMapper.java
@@ -0,0 +1,20 @@
+package com.aicodinator.backend.domain.community.mapper;
+
+import com.aicodinator.backend.domain.community.domain.dto.response.CommentResponse;
+import com.aicodinator.backend.domain.community.domain.entity.Comment;
+import com.aicodinator.backend.domain.user.domain.entity.User;
+import org.mapstruct.Context;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface CommentMapper {
+    @Mapping(source = "id", target = "commentId")
+    @Mapping(source = "user.name", target = "authorName")
+    @Mapping(target = "isOwner", expression = "java(comment.getUser().equals(currentUser))")
+    CommentResponse toCommentResponse(Comment comment, @Context User currentUser);
+
+    List<CommentResponse> toCommentResponseList(List<Comment> commentList);
+}
