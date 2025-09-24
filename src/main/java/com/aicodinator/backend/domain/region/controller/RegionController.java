8d53e2f55c57307a347906ea829aaa8c1025dbaa
Yooonjeong <wkddbs3935@naver.com>
2025-09-24 21:47:55 +0900

    feat: 사용자별 추천받은 지역 조회 API 개발

--- src/main/java/com/aicodinator/backend/domain/region/controller/RegionController.java
+++ src/main/java/com/aicodinator/backend/domain/region/controller/RegionController.java
@@ -1,11 +1,26 @@
 package com.aicodinator.backend.domain.region.controller;
 
+import com.aicodinator.backend.domain.region.domain.Region;
 import com.aicodinator.backend.domain.region.service.RegionService;
+import com.aicodinator.backend.domain.user.domain.entity.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/regions")
 public class RegionController {
     private final RegionService regionService;
+
+    @GetMapping("/my")
+    public ResponseEntity<List<Region>> findMyRegions(@AuthenticationPrincipal User user) {
+        return ResponseEntity.ok(regionService.findMyRegions(user));
+    }
+    // TODO: CustomOAuth2UserPrincipal로 변경
 }
--- src/main/java/com/aicodinator/backend/domain/region/service/RegionService.java
+++ src/main/java/com/aicodinator/backend/domain/region/service/RegionService.java
@@ -1,11 +1,26 @@
 package com.aicodinator.backend.domain.region.service;
 
+import com.aicodinator.backend.domain.region.domain.Region;
 import com.aicodinator.backend.domain.region.repository.RegionRepository;
+import com.aicodinator.backend.domain.user.domain.entity.User;
+import com.aicodinator.backend.global.exception.CustomException;
+import com.aicodinator.backend.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class RegionService {
     private final RegionRepository regionRepository;
+
+    public Region findById(Long regionId) {
+        return regionRepository.findById(regionId)
+            .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND, "요청하신 지역을 찾을 수 없습니다."));
+    }
+
+    public List<Region> findMyRegions(User user) {
+        return regionRepository.findRegionsByUser(user);
+    }
 }
