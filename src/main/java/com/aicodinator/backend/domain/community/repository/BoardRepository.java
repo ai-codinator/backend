9e5b86d4887a530ea1b2b0d48784e107e6045cc5
Yooonjeong <wkddbs3935@naver.com>
2025-09-24 21:52:07 +0900

    feat: Board CRUD 비즈니스 로직 구현

--- src/main/java/com/aicodinator/backend/domain/community/repository/BoardRepository.java
+++ src/main/java/com/aicodinator/backend/domain/community/repository/BoardRepository.java
@@ -1,9 +1,29 @@
 package com.aicodinator.backend.domain.community.repository;
 
 import com.aicodinator.backend.domain.community.domain.entity.Board;
+import com.aicodinator.backend.domain.region.domain.Region;
+import com.aicodinator.backend.domain.user.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board,Long> {
+    List<Board> findByRegion(Region region);
+
+    List<Board> findByRegionId(Long regionId);
+
+    /**
+     * 특정 사용자가 속한 지역들의 모든 게시판 조회
+     * @param user
+     * @return 게시판 리스트
+     */
+    @Query("SELECT b FROM Board b WHERE b.region IN " +
+        "(SELECT ur.region FROM UserRegion ur WHERE ur.user = :user)")
+    List<Board> findBoardsForUserRegions(@Param("user") User user);
+
+    Long region(Region region);
 }
--- src/main/java/com/aicodinator/backend/domain/community/service/BoardService.java
+++ src/main/java/com/aicodinator/backend/domain/community/service/BoardService.java
@@ -1,11 +1,51 @@
 package com.aicodinator.backend.domain.community.service;
 
+import com.aicodinator.backend.domain.community.domain.constant.BoardType;
+import com.aicodinator.backend.domain.community.domain.entity.Board;
 import com.aicodinator.backend.domain.community.repository.BoardRepository;
+import com.aicodinator.backend.domain.region.domain.Region;
+import com.aicodinator.backend.domain.user.domain.entity.User;
+import com.aicodinator.backend.global.exception.CustomException;
+import com.aicodinator.backend.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class BoardService {
     private final BoardRepository boardRepository;
+
+    public Board findById(long id) {
+        return boardRepository.findById(id)
+            .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND, "요청하신 게시판을 찾을 수 없습니다."));
+    }
+
+    public List<Board> findByRegion(Region region) {
+        return boardRepository.findByRegion(region);
+    }
+
+    public List<Board> findByRegionId(Long regionId) {
+        return boardRepository.findByRegionId(regionId);
+    }
+
+    public List<Board> findByUser(User user) {
+        return boardRepository.findBoardsForUserRegions(user);
+    }
+
+    public void createBoards(Region region) {
+        if (findByRegion(region).size() >= 4) return;
+
+        List<BoardType> boardTypes = List.of(BoardType.FREE, BoardType.QNA, BoardType.REVIEW, BoardType.PROMO);
+
+        List<Board> boards = boardTypes.stream()
+            .map(boardType ->
+                Board.builder()
+                    .region(region)
+                    .boardType(boardType)
+                    .build())
+            .toList();
+        boardRepository.saveAll(boards);
+    }
 }
