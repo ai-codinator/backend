49a2ba7def08aca8570ce26663c1b50746645f3c
Yooonjeong <wkddbs3935@naver.com>
2025-09-24 21:54:16 +0900

    feat: Post Mapper 정의

---
+++ src/main/java/com/aicodinator/backend/domain/community/mapper/PostMapper.java
@@ -0,0 +1,28 @@
+package com.aicodinator.backend.domain.community.mapper;
+
+import com.aicodinator.backend.domain.community.domain.dto.response.PostDetailResponse;
+import com.aicodinator.backend.domain.community.domain.dto.response.PostListResponse;
+import com.aicodinator.backend.domain.community.domain.entity.Post;
+import com.aicodinator.backend.domain.user.domain.entity.User;
+import org.mapstruct.Context;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+@Mapper(
+    componentModel = "spring",
+    uses = {PostFileMapper.class, CommentMapper.class}
+)
+public interface PostMapper {
+    @Mapping(source = "id", target = "postId")
+    @Mapping(source = "user.name", target = "authorName")
+    @Mapping(target = "isOwner", expression = "java(post.getUser().equals(currentUser))")
+    PostDetailResponse toPostDetailResponse(Post post, @Context User currentUser);
+
+    @Mapping(source = "id", target = "postId")
+    PostListResponse toPostListResponse(Post post);
+
+    List<PostListResponse> toPostListResponseList(List<Post> posts);
+}
