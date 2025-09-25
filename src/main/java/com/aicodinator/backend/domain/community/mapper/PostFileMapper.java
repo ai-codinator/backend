72e905587745ba50618fbe8e0d7b9f85a3d092bc
Yooonjeong <wkddbs3935@naver.com>
2025-09-25 19:39:17 +0900

    feat: PostFileMapper에서 의존성 필드 주입 -> setter 방식으로 변경

--- src/main/java/com/aicodinator/backend/domain/community/mapper/PostFileMapper.java
+++ src/main/java/com/aicodinator/backend/domain/community/mapper/PostFileMapper.java
@@ -2,22 +2,34 @@ package com.aicodinator.backend.domain.community.mapper;
 
 import com.aicodinator.backend.domain.community.domain.dto.response.PostFileResponse;
 import com.aicodinator.backend.domain.community.domain.entity.PostFile;
+import lombok.extern.slf4j.Slf4j;
 import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
+import java.util.List;
+
+@Slf4j
 @Mapper(componentModel = "spring")
 public abstract class PostFileMapper {
-    @Value("${cloud.aws.s3.base-url}")
-    private String s3BaseUrl;
+    protected String s3BaseUrl;
+
+    @Autowired
+    public void setS3BaseUrl(@Value("${cloud.aws.s3.base-url}") String s3BaseUrl) {
+        log.info("S3 Base URL Injected: {}", s3BaseUrl);
+        this.s3BaseUrl = s3BaseUrl;
+    }
 
     @Mapping(source = "id", target = "fileId")
     public abstract PostFileResponse toPostFileResponse(PostFile postFile);
 
     @AfterMapping
-    protected void afterMapping(PostFile postFile, @MappingTarget PostFileResponse response) {
-        response.setFileUrl(s3BaseUrl + postFile.getStoredKey());
+    protected void afterMapping(PostFile postFile, @MappingTarget PostFileResponse.PostFileResponseBuilder responseBuilder) {
+        log.info("s3BaseUrl in afterMapping: {}", s3BaseUrl);
+        log.info("StoredKey in afterMapping: {}", postFile.getStoredKey());
+        responseBuilder.fileUrl(s3BaseUrl + postFile.getStoredKey());
     }
 }
